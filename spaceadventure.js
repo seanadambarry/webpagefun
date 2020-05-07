fe868c08a338df2d2d6d9b78237a899bd7561b7e
seanadambarry <63555354+seanadambarry@users.noreply.github.com>
2020-05-07 15:51:54 -0400

    addddsss

--- spaceadventure.js
+++ spaceadventure.js
@@ -27,11 +27,31 @@ let spaceShip = {
 
 let myself = {
     name : 'Sean',
-    age : 41,
-    health : 100,
+    _age : 41,
+    _health : 100,
+    get health(){
+        //if health is lower than 20, just say it's 'low'
+        if (this._health < 20){
+            return 'Low'
+        } else {
+            return this._health;
+        }
+    },
     hunger : 0,
     strength : 50,
+    set age(newAge){
+        //check to see if the prompt from userCommand() !isNaN
+        if (!isNaN(newAge)){
+          this._age = newAge;
+        } else {
+            alert('You must assign a number to age');
+            console.log('You must assign a number to age');
+            myself.age = Number(prompt("What is your new age?"));
+        }
+      }
 }
+
+let lastCommand = '';
 
 for (let key in spaceShip) {
     console.log(key, spaceShip[key])
@@ -44,16 +64,21 @@ for (let key in spaceShip) {
 
 function userCommand() {
     let entCom;
-    entCom = document.getElementById("userInput").value;
+    //make value from text entry box all lowercase
+    entCom = (document.getElementById("userInput").value).toLowerCase();
+    lastCommand = entCom
     console.log(entCom)
     document.getElementById("lastcommand1").innerHTML = entCom;
-    if (entCom === 'mystats'){
+    if (entCom === 'my stats'){
+        //displays the persons stats from the myself object
         document.getElementById("displayArea").innerHTML = `Your stats are to the right`;
         document.getElementById("name").innerHTML = myself.name;
-        document.getElementById("age").innerHTML = myself.age;
+        document.getElementById("age").innerHTML = myself._age;
         document.getElementById("health").innerHTML = myself.health;
         document.getElementById("hunger").innerHTML = myself.hunger;
         document.getElementById("strength").innerHTML = myself.strength;
+    } else if (entCom === 'change age'){
+        myself.age = Number(prompt("What is your new age?"));
     }
 };
 
@@ -71,5 +96,9 @@ inputField.addEventListener("keyup", function(event) {
     //after pressing enter, clear the text box --maybe add fucntonality to log the previous commands below with a loop etc
     document.getElementById("userInput").value = "";
     
+  } else if (event.keyCode === 38) {
+    document.getElementById("userInput").value = lastCommand
+  } else if (event.keyCode === 27) {
+    document.getElementById("userInput").value = ""
   }
 });
