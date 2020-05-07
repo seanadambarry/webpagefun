6c4119d7a3577901502368b4e65a945755aa8498
seanadambarry <63555354+seanadambarry@users.noreply.github.com>
2020-05-07 03:16:19 -0400

    push

--- spaceadventure.js
+++ spaceadventure.js
@@ -1,8 +1,15 @@
+"use strict"
+
+window.onload = function() {
+    document.getElementById("userInput").focus();
+  };
+
 let spaceShip = {
     name: 'RickMobile',
     systems: {
         engineering: {
-            manager: 'Rick'
+            manager: 'Rick',
+            powerlevel: 50
         },
         combat: {
             manager: 'Summer'
@@ -18,8 +25,51 @@ let spaceShip = {
     fueltype: 'green death crystals'
 }
 
+let myself = {
+    name : 'Sean',
+    age : 41,
+    health : 100,
+    hunger : 0,
+    strength : 50,
+}
 
-for ( key in spaceShip) {
+for (let key in spaceShip) {
     console.log(key, spaceShip[key])
     
-}
+};  
+
+
+
+
+
+function userCommand() {
+    let entCom;
+    entCom = document.getElementById("userInput").value;
+    console.log(entCom)
+    document.getElementById("lastcommand1").innerHTML = entCom;
+    if (entCom === 'mystats'){
+        document.getElementById("displayArea").innerHTML = `Your stats are to the right`;
+        document.getElementById("name").innerHTML = myself.name;
+        document.getElementById("age").innerHTML = myself.age;
+        document.getElementById("health").innerHTML = myself.health;
+        document.getElementById("hunger").innerHTML = myself.hunger;
+        document.getElementById("strength").innerHTML = myself.strength;
+    }
+};
+
+// Get the input field
+var inputField = document.getElementById("userInput");
+
+// Execute a function when the user releases a key on the keyboard
+inputField.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("userCommandButton").click();
+    //after pressing enter, clear the text box --maybe add fucntonality to log the previous commands below with a loop etc
+    document.getElementById("userInput").value = "";
+    
+  }
+});
