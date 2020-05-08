55d6ec2995759c75c9b5d9a791c8a2f3e208f9b1
seanadambarry <63555354+seanadambarry@users.noreply.github.com>
2020-05-07 20:10:34 -0400

    fun fun

--- spaceadventure.js
+++ spaceadventure.js
@@ -26,9 +26,9 @@ let spaceShip = {
 }
 
 let myself = {
-    name : 'Sean',
+    name : 'Rick',
     _age : 41,
-    _health : 100,
+    _health : 70,
     get health(){
         //if health is lower than 20, just say it's 'low'
         if (this._health < 20){
@@ -37,7 +37,7 @@ let myself = {
             return this._health;
         }
     },
-    hunger : 0,
+    hunger : 'I could eat something',
     strength : 50,
     set age(newAge){
         //check to see if the prompt from userCommand() !isNaN
@@ -53,13 +53,6 @@ let myself = {
 
 let lastCommand = '';
 
-/*
-for (let key in spaceShip) {
-    console.log(key, spaceShip[key])
-    
-};  
-*/
-
 //monster attributes
 const monsterTypes = [
     'Ollie', 'Andi', 'Sypereka', 'Panda', 'Flirt'
@@ -67,11 +60,61 @@ const monsterTypes = [
 const monsterStrength = [
     90, 70, 88, 77, 66
 ]
+
+
+
+let consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+let vowels = ['a','e','i','o','u'];
+
+//totally randomize a monster name
+let monsterName = [];
+for (let m = 0; m < 100; m++){
+    let randomnum = Math.floor(Math.random() * 100);
+    if (randomnum <= 20){
+        //name is 5 char long
+        monsterName[m] = consonants[Math.floor(Math.random() * consonants.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+    }else if (randomnum >=21 && randomnum <= 40){
+        //name is 6 char long
+        monsterName[m] = consonants[Math.floor(Math.random() * consonants.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+    
+    }else if (randomnum >= 41 && randomnum <= 60) {
+        //name is 3 char long
+        monsterName[m] = vowels[Math.floor(Math.random() * vowels.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+    } else if (randomnum >= 61 && randomnum <=70){
+        //name is 4 char long plus a number from 1 to 100
+        monsterName[m] = vowels[Math.floor(Math.random() * vowels.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + Math.floor(Math.random() * 100)
+    } else if (randomnum >= 71 && randomnum <= 90 ){
+        monsterName[m] = vowels[Math.floor(Math.random() * vowels.length)]
+        + 'joe'
+        + consonants[Math.floor(Math.random() * consonants.length)]
+    } else {
+        monsterName[m] = vowels[Math.floor(Math.random() * vowels.length)]
+        + 'massi'
+        + consonants[Math.floor(Math.random() * consonants.length)]
+        + vowels[Math.floor(Math.random() * vowels.length)]
+    }
+}
+
 //monster generator
 let monsters = [];
 for (let m = 0; m < 100; m++){
     monsters[m] = {
         //randomize type and strength
+        name: monsterName[Math.floor(Math.random() * monsterName.length)],
         type : monsterTypes[Math.floor(Math.random() * monsterTypes.length)],
         strength : monsterStrength[Math.floor(Math.random() * monsterTypes.length)],
         health : Math.floor(Math.random() * 50) + 50
@@ -79,11 +122,38 @@ for (let m = 0; m < 100; m++){
     
 }
 console.log(monsters)
+console.log(monsterName)
 
-//current enemy and it is random from the monsters array
 let currentenemy;
-currentenemy = monsters[Math.floor(Math.random() * 100)]
-console.log(currentenemy)
+
+//show hunger stat since you would feel hungry even if you didn't know anything else
+document.getElementById("health").innerHTML = myself.health;
+document.getElementById("hunger").innerHTML = myself.hunger;
+
+// Get the input field
+var inputField = document.getElementById("userInput");
+// Execute a function when the user releases a key on the keyboard
+inputField.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("userCommandButton").click();
+    //after pressing enter, clear the text box --maybe add fucntonality to log the previous commands below with a loop etc
+    document.getElementById("userInput").value = "";
+    
+  } else if (event.keyCode === 38) {
+    document.getElementById("userInput").value = lastCommand
+  } else if (event.keyCode === 27) {
+    document.getElementById("userInput").value = ""
+  }
+});
+
+//get a new enemy that are monsters from the monster array
+function getNewEnemy() {
+    return monsters[Math.floor(Math.random() * 100)]
+}
 
 function userCommand() {
     let entCom;
@@ -102,29 +172,14 @@ function userCommand() {
         document.getElementById("strength").innerHTML = myself.strength;
     } else if (entCom === 'change age'){
         myself.age = Number(prompt("What is your new age?"));
-    } else if (entCom === 'enemy'){
+    } else if (entCom === 'who am i'){
+        document.getElementById("name").innerHTML = myself.name;
+    } else if (entCom === 'new enemy'){
+        currentenemy = getNewEnemy()
+        document.getElementById("ename").innerHTML = currentenemy.name;
         document.getElementById("etype").innerHTML = currentenemy.type;
+        document.getElementById("ehealth").innerHTML = currentenemy.health;
         document.getElementById("estrength").innerHTML = currentenemy.strength;
+        console.log(currentenemy)
     }
 };
-
-// Get the input field
-var inputField = document.getElementById("userInput");
-
-// Execute a function when the user releases a key on the keyboard
-inputField.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("userCommandButton").click();
-    //after pressing enter, clear the text box --maybe add fucntonality to log the previous commands below with a loop etc
-    document.getElementById("userInput").value = "";
-    
-  } else if (event.keyCode === 38) {
-    document.getElementById("userInput").value = lastCommand
-  } else if (event.keyCode === 27) {
-    document.getElementById("userInput").value = ""
-  }
-});
