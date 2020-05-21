48046a6e9f6b029d483c930d5134d4678847064c
seanadambarry <63555354+seanadambarry@users.noreply.github.com>
2020-05-21 16:33:41 -0400

    terrbile

--- spaceship.js
+++ spaceship.js
@@ -1,11 +1,17 @@
 var canvas = document.getElementById("spaceship");
 var ctx = canvas.getContext("2d");
-var img = document.getElementById("image");
+var img1 = document.getElementById("image1");
+var img2 = document.getElementById("image2");
+var img3 = document.getElementById("image3");
+
 
 
 let randomy1 = Math.floor(Math.random() * 75) + 100
 let xvalue1 = 300;
+
+
 function starOne(){
+    console.log('star 1')
     ctx.beginPath()
     ctx.moveTo(xvalue1,randomy1)
     ctx.strokeStyle = "white"
@@ -17,7 +23,7 @@ function starOne(){
     if (xvalue1 < -150){
         
         randomy1 = Math.floor(Math.random() * 300)
-        console.log('random y1 value' + randomy1)
+        
         xvalue1 = 600;
     }
 }
@@ -25,6 +31,7 @@ function starOne(){
 let randomy2 = Math.floor(Math.random() * 75) + 100
 let xvalue2 = 500;
 function starTwo(){
+    console.log('star 2')
     ctx.beginPath()
     ctx.moveTo(xvalue2,randomy2)
     ctx.strokeStyle = "white"
@@ -36,7 +43,7 @@ function starTwo(){
     if (xvalue2 === -100){
         
         randomy2 = Math.floor(Math.random() * 300)
-        console.log('random y2 value' + randomy2)
+       
         xvalue2 = 600;
     }
 }
@@ -44,6 +51,7 @@ function starTwo(){
 let randomy3 = Math.floor(Math.random() * 33) + 100
 let xvalue3 = 100;
 function starThree(){
+    console.log('star 3')
     ctx.beginPath()
     ctx.moveTo(xvalue3,randomy3)
     ctx.strokeStyle = "white"
@@ -55,7 +63,7 @@ function starThree(){
     if (xvalue3 < -100){
         
         randomy3 = Math.floor(Math.random() * 300)
-        console.log('random y3 value' + randomy3)
+      
         xvalue3 = 600;
     }
 }
@@ -64,6 +72,7 @@ function starThree(){
 let randomy4 = Math.floor(Math.random() * 44) + 100
 let xvalue4 = 222;
 function starFour(){
+    console.log('star 4')
     ctx.beginPath()
     ctx.moveTo(xvalue4,randomy4)
     ctx.strokeStyle = "white"
@@ -73,25 +82,38 @@ function starFour(){
     ctx.lineTo(xvalue4, randomy4)
     
     ctx.stroke()
-    console.log(xvalue4)
+
     xvalue4 = xvalue4 - 2
 
     if (xvalue4 < -50){
         
         randomy4 = Math.floor(Math.random() * 222)
-        console.log('random y4 value' + randomy4)
+        
         xvalue4 = 600;
-        console.log(xvalue4)
+     
+    }
+}
+let ssInt = 1;
+function spaceShip(){
+    if (ssInt === 1){
+        ctx.drawImage(img1, 100, 0); 
+        ssInt++
+        console.log('image 1')
+    } else if (ssInt === 2){
+        ctx.drawImage(img2, 100, 0);  
+        ssInt++
+        console.log('image 2')
+    } else if (ssInt === 3){
+        ctx.drawImage(img3, 100, 0);
+        ssInt = 1;  
+        console.log('image 3')
     }
 }
 
-function spaceShip(){
-    ctx.drawImage(img, 200, 75);  
-}
-
 function animateStars(){
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#1c1c1c";
     ctx.fillRect(0,0,600,300)
+    console.log('rect fill')
 
     starOne()
     starTwo()
