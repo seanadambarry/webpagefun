0f486963e7e341c7a226ed40eb6940e86ae5e432
seanadambarry <63555354+seanadambarry@users.noreply.github.com>
2021-01-06 12:41:30 -0500

    newfun

---
+++ mrmeeseeks.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let randomword1 = allwords[Math.floor(Math.random() * allwords.length)];
+let randomword2 = allwords[Math.floor(Math.random() * allwords.length)];
+let randomword3 = allwords[Math.floor(Math.random() * allwords.length)];
+let randomword4 = allwords[Math.floor(Math.random() * allwords.length)];
+let randomword5 = allwords[Math.floor(Math.random() * allwords.length)];
+
+
+
+document.getElementById("randomword1").innerHTML = randomword1;
+document.getElementById("randomword2").innerHTML = randomword2;
+document.getElementById("randomword3").innerHTML = randomword3;
+document.getElementById("randomword4").innerHTML = randomword4;
+document.getElementById("randomword5").innerHTML = randomword5;
