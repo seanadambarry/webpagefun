b5a43825cb95fe02c2363ac4dc9f3b43ed1b3ca6
seanadambarry <63555354+seanadambarry@users.noreply.github.com>
2021-09-17 16:58:54 -0400

    new fdp calc

---
+++ FDPCALC.js
@@ -0,0 +1,60 @@
+"use strict"
+let averageft = "50p";
+let numberofflts = 1;
+let startoffdp = "24:00-03:59"
+console.log(averageft + ' averageft default')
+console.log(numberofflts + ' numberofflts default')
+console.log(startoffdp + ' startoffdp default')
+document.getElementById("output").innerHTML = averageft + " " + numberofflts + " " + startoffdp
+
+
+function calculate() {
+    averageft = document.getElementById("averageft").value;
+    numberofflts = parseInt(document.getElementById("numberofflts").value)
+    startoffdp = document.getElementById("startoffdp").value
+
+    console.log(averageft + ' averageft')
+    console.log(numberofflts + ' numberofflts')
+    console.log(startoffdp + ' startoffdp')
+    console.log(typeof numberofflts)
+
+    switch(averageft){
+        case "50p":
+            console.log("50 plus case");
+            if (numberofflts < 5){
+                console.log("1-4 flights")
+            } else if (numberofflts > 4 & numberofflts < 7){
+                console.log("5-6 flights")
+            } else if (numberofflts >= 7){
+                console.log("7 or more flights")
+            } else {
+                console.log("error")
+            }
+            break;
+        case "30m-l50":
+            console.log("30 mins or more less than 50");
+            if (numberofflts < 8){
+                console.log("1-7 flights")
+            } else if (numberofflts > 7 & numberofflts < 12){
+                console.log("8-11 flights")
+            } else if (numberofflts >= 12){
+                console.log("12 or more flights")
+            } else {
+                console.log("error")
+            }
+            break;
+        case "l30":
+            console.log("less than 30");
+            if (numberofflts < 12){
+                console.log("1-11 flights")
+            } else if (numberofflts > 11 & numberofflts < 18){
+                console.log("12-17 flights")
+            } else if (numberofflts >= 18){
+                console.log("18 or more flights")
+            } else {
+                console.log("error")
+            }
+            break;
+    }
+};
+
--- mrmeeseeks.js
+++ mrmeeseeks.js
@@ -13,3 +13,34 @@ document.getElementById("randomword2").innerHTML = randomword2;
 document.getElementById("randomword3").innerHTML = randomword3;
 document.getElementById("randomword4").innerHTML = randomword4;
 document.getElementById("randomword5").innerHTML = randomword5;
+
+let inputField = document.getElementById("userInput");
+// Execute a function when the user releases a key on the keyboard
+inputField.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    userEntry()
+    //after pressing enter, clear the text box --maybe add fucntonality to log the previous commands below with a loop etc
+    document.getElementById("userInput").value = "";
+    
+  } else if (event.keyCode === 38) {
+ 
+  } else if (event.keyCode === 27) {
+    document.getElementById("userInput").value = ""
+  }
+});
+
+//this is the main user interface where the user enters commands.
+
+function userEntry() {
+    let entCom = (document.getElementById("userInput").value).toLowerCase();
+    let wordExists = allwords.includes(entCom, 0);
+ 
+
+    if (wordExists === true){
+      document.getElementById("testareablank").innerHTML = "yes, this word exists";
+    } else {
+      document.getElementById("testareablank").innerHTML = "no, this words does not exist";
+    }
+}
